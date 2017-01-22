9085500ef6c66975aa063c5764dc41199867b576
Christopher Chen <Christopher Chen>
2017-01-22 15:45:46 -0500

    Initial commit with basic OData client proxy

---
+++ Nexus/Nexus.Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Linq;
+
+namespace Nexus.Droid
+{
+	[Activity (Label = "Nexus.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+            var myClass = new MyClass();
+			button.Click += async delegate
+            {
+                var result = await myClass.TestGet();
+                var list = result.ToList();
+                button.Text = string.Format("{0} clicks! {1}", count++, String.Join(", ", list.Select(x => x.Name)));
+            };
+		}
+	}
+}
+
+
---
+++ Nexus/Nexus/MyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Nexus.Api.Intelex;
+using Nexus.Api.Default;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nexus
+{
+    public class MyClass
+    {
+        private readonly Container Container;
+        public MyClass()
+        {
+            Container = new Container(new Uri("http://ilx00385.intelex.com/devChris/api/v2/object"));
+            Container.SendingRequest2 += Container_SendingRequest2;
+        }
+
+        public async Task<IEnumerable<Nexus_AssociationsObject>> TestGet()
+        {
+            try
+            {
+                var result = await Container.Nexus_AssociationsObject.ExecuteAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        private void Container_SendingRequest2(object sender, Microsoft.OData.Client.SendingRequest2EventArgs e)
+        {
+            var authHeaderValue = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(@"INTELEX\Administrator:Administrator"));
+
+            e.RequestMessage.SetHeader("Authorization", "Basic " + authHeaderValue); //this is where you pass the creds.
+
+        }
+    }
+}
+
